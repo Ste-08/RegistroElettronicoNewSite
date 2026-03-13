fce54acc1010e54f7726effc8d77dd27e6334fc2
Ste-08 <cozzulastefano@gmail.com>
2026-03-13 21:08:27 +0100

    fix: usa text nel bottone login per evitare errore visible su string

--- views/login_view.py
+++ views/login_view.py
@@ -30,7 +30,7 @@ class LoginView(ft.Column):
         )
         
         self.login_button = ft.ElevatedButton(
-            content="Accedi",
+            text="Accedi",
             width=300,
             height=50,
             style=ft.ButtonStyle(
